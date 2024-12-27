dec0f906e355f96f350e0969deb025514b286ad0
Patrik Revesz <rvs.patrik.2003@gmail.com>
2024-12-27 19:50:02 +0100

    first  commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Django & React Web App 
+
+[text](https://www.youtube.com/watch?v=c-QsfbznSXI)
+
+I'm at 45:00
---
+++ TMT_WebApp/frontend/src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+function NotFound(){
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>Dumbass</p>
+    </div>
+}
+export default NotFound
